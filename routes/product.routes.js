3ab9d8019948eaca7b304ccb8524c8b49f986d7f
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-03 14:42:41 +0100

    implemented Product routes

---
+++ routes/product.routes.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const router = express.Router();
+
+const prisma = require("../db/index");
+
+router.post("/products", async (req, res, next) => {
+  const { name, description, price, quantity, category, imageUrl, seller } =
+    req.body;
+
+  const newProduct = {
+    name,
+    description,
+    price,
+    quantity,
+    category,
+    imageUrl,
+    seller,
+  };
+  try {
+    const product = await prisma.product.create({ data: newProduct });
+    res.status(201).json(product);
+  } catch (error) {
+    console.log("Error in POST // api/products", error);
+    res.status(500).json({ message: "Error creating new product" });
+  }
+});
+
+router.get("/products", async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log("Error in GET // api/products", error);
+    res.status(500).json({ message: "Error getting products from DB" });
+  }
+});
+
+router.get("/products/:productId", async (req, res, next) => {
+  const { productId } = req.params;
+  try {
+    const product = await prisma.product.findUnique({
+      where: { id: productId },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    console.log("Error in GET // api/products/productID", error);
+    res.status(500).json({ message: "Error getting product from DB" });
+  }
+});
+
+router.patch("/products/:productId", async (req, res, next) => {
+  const { productId } = req.params;
+
+  const { name, description, price, quantity, category, imageUrl, seller } =
+    req.body;
+
+  const updatedProduct = {
+    name,
+    description,
+    price,
+    quantity,
+    category,
+    imageUrl,
+    seller,
+  };
+  try {
+    const product = await prisma.product.update({
+      where: { id: productId },
+      data: updatedProduct,
+    });
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log("Error in PATCH // api/products/productId", error);
+    res.status(500).json({ message: "Error updating product" });
+  }
+});
+
+router.delete("/products/:productId", async (req, res, next) => {
+  const { productId } = req.params;
+
+  try {
+    const product = prisma.product.delete({ where: { id: productId } });
+    res.status(200).json({
+      message: `${product.name} with id of ${productId} has been deleted successfully`,
+    });
+  } catch (error) {
+    console.log("Error in DELETE // api/products/productId", error);
+    res.status(500).json({ message: "Error deleting product from DB" });
+  }
+});
+
+module.exports = router;
